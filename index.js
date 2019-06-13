fd608310bb0ed6aed5931faa2781ac4acc47611d
Weslley Braga <wesbragagt@gmail.com>
2019-06-12 23:41:55 -0500

    displaying the word count as well as the words array

--- index.js
+++ index.js
@@ -4,25 +4,27 @@ const preamble =
 // First we need a function that will parse our date and give us the output needed based on a certain set of arguments.
 
 function wordCount(startLetter = "t", endLetter = "e", string) {
-    // we split our text string into seperate words inside an array that can be manipulated
+    // we split our text string into separate words inside an array that can be manipulated
 
     const wordsArray = string.toLowerCase().split(/[ ,.]+/);
+    // PROBLEM
+    // At first I splitted the string by spaces and realized that I wouldn't be able to compare an ending letter with a comma for example, or period, so by using a regular expression I can split the data separated by spaces, commas and periods.
 
-    // At first I splitted the string by spaces and realized that I wouldn't be able to compare an ending letter with a comman for example, or period, so by using a regular expression I can split the data separated by spaces, commas and periods.
+    // PROBLEM
+    // "e" !== "E"
+    // I found very helpful to parse each word to lower case characters at first in order to make it easier to compare them later.
 
-    // I also found very helpful to parse all of ours words to lower case characters at first in order to make it easier to compare later on.
+    // Here I loop through the array and return another array with only elements that pass a certain condition.
 
-    // Here we loop through the array and return another array with only elements that pass a certain condition.
-
-    const startWith = wordsArray.filter(function(word) {
+    const startWith = wordsArray.filter(word => {
         return word[0] === startLetter;
     });
 
-    const endWith = wordsArray.filter(function(word) {
+    const endWith = wordsArray.filter(word => {
         return word[word.length - 1] === endLetter;
     });
 
-    const startAndEndWith = wordsArray.filter(function(word) {
+    const startAndEndWith = wordsArray.filter(word => {
         return word[0] === startLetter && word[word.length - 1] === endLetter;
     });
 
@@ -30,17 +32,17 @@ function wordCount(startLetter = "t", endLetter = "e", string) {
     console.log(
         ` --------------------------------------------------------------------------------------- \n starting with the letter (${startLetter}) we have a total of ${
             startWith.length
-        } word(s).`
+        } word(s). \n ${startWith}`
     );
     console.log(
         `--------------------------------------------------------------------------------------- \n  ending with the letter (${endLetter}) we have a total of ${
             endWith.length
-        } word(s).`
+        } word(s). \n ${endWith}`
     );
     console.log(
         `--------------------------------------------------------------------------------------- \n  starting with the letter (${startLetter}) and ending with the letter (${endLetter}) we have a total of ${
             startAndEndWith.length
-        } word(s).`
+        } word(s). \n ${startAndEndWith}`
     );
 }
 // Making our application more dynamic so the user can pass different letters as arguments to our node execution command.
